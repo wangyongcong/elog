048b2e074cc0f5fc602a1d992831e0208992b4dd
wangyongcong <wangyongcong@gmail.com>
2014-03-09 16:47:52 +0800

    Modify project structure

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,8 +1,7 @@
-// elog.cpp : 定义控制台应用程序的入口点。
+// Test.cpp : 定义控制台应用程序的入口点。
 //
 
 #include "stdafx.h"
-
 #include <thread>
 #include <ctime>
 #include "logger.h"
@@ -10,7 +9,7 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 	wyc::xlogger log;
-	log.create("test",NULL,1024*1024,wyc::LOG_WARN);
+	log.create("test", NULL, 1024 * 1024, wyc::LOG_WARN);
 	const char *dummy_info = "This is a dummy message for logger debugging.";
 	std::chrono::milliseconds ms(33);
 	unsigned cnt = 0;
@@ -19,8 +18,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		for (int j = 0; j < 100; ++j)
 		{
 			log.debug("[%d] %s", ++cnt, dummy_info);
-			log.info ("[%d] %s", ++cnt, dummy_info);
-			log.warn ("[%d] %s", ++cnt, dummy_info);
+			log.info("[%d] %s", ++cnt, dummy_info);
+			log.warn("[%d] %s", ++cnt, dummy_info);
 			log.error("[%d] %s", ++cnt, dummy_info);
 			log.fatal("[%d] %s", ++cnt, dummy_info);
 		}
